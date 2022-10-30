42637c5ffce920592512749e6e537a9b78c2e8ac
garcialucasm <116840436+garcialucasm@users.noreply.github.com>
2022-10-30 02:22:31 +0000

    Delete exercise_20.py

--- exercise_20.py
+++
@@ -1,7 +0,0 @@
-from random import sample
-
-n1 = str(input('Type the first name:'))
-n2 = str(input('Type the second name:'))
-n3 = str(input('Type the third name:'))
-a_lista  = [n1, n2, n3]
-print('Random order : {}' .format(sample(a_lista,3)))
