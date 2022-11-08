f08c0c9f007092b0a1403696424b897a69f27729
garcialucasm <116840436+garcialucasm@users.noreply.github.com>
2022-11-08 21:57:17 +0000

    Add files via upload

---
+++ exercicio_51.py
@@ -0,0 +1,8 @@
+primeiro = int(input('Digite o primeiro termo: '))
+r = int(input('Digite a razão: '))
+list_sum = list()
+for c in range(primeiro,primeiro+r*10, r):
+    list_sum.append(c)
+    print(c, end=(' --> '))
+print('Acabou')
+print('\n', list_sum)
