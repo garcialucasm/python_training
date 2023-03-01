a6130ad63ec9ee400052b6e93d66f39cc837f10d
garcialucasm <116840436+garcialucasm@users.noreply.github.com>
2023-03-01 20:22:36 +0000

    added comments to exercise_017

--- exercise_017.py
+++ exercise_017.py
@@ -1,6 +1,9 @@
+# Calculate Hypotenuse
+# Read two sides of a square rectangle and calculete the hypotenuse
+
 from math import hypot
-catop = float(input('Opposite: '))
-catad = float(input('Adjacent: '))
-Hypotenuse = hypot(catop, catad)
-print('Hypotenuse: {:.2f}'.format(Hypotenuse))
-print(type(Hypotenuse))
+op_collar = float(input('Opposite: '))
+ad_collar = float(input('Adjacent: '))
+hypotenuse = hypot(catop, catad)
+print('Hypotenuse: {:.2f}'.format(hypotenuse))
+print(type(hypotenuse))
